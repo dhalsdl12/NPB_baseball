298b5de0505d887966952d6210114d7c58fab09e
Ohmin Kwon <71929898+dhalsdl12@users.noreply.github.com>
2025-09-08 21:59:29 +0900

    Update npb_standings.py

--- npb_standings.py
+++ npb_standings.py
@@ -16,17 +16,17 @@ def extract_standings(driver, url_c, url_p):
 
     # --- Central League ---
     driver.get(url_c)
-    central_prefix = '//*[@id="stdivmaintbl"]/table/tbody/tr/td/div[1]/table/tbody/tr[2]/td[1]/table/tbody/tr/td'
+    central_prefix = '//*[@id="stdivmaintbl"]/table/tbody/tr/td/div[1]/table/tbody/tr'
     central_rows = driver.find_elements(By.XPATH, central_prefix + '/..')
-    for i in range(1, len(central_rows)+1):
+    for i in range(2, len(central_rows)+2):
         try:
-            team = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[2]').text.strip()
-            game = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[3]').text.strip()
-            win = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[4]').text.strip()
-            lose = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[5]').text.strip()
-            tie = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[6]').text.strip()
-            pct = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[7]').text.strip()
-            gb = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[8]').text.strip()
+            team = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[1]table/tbody/tr/td[2]').text.strip()
+            game = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[2]').text.strip()
+            win = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[3]').text.strip()
+            lose = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[4]').text.strip()
+            tie = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[5]').text.strip()
+            pct = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[6]').text.strip()
+            gb = driver.find_element(By.XPATH, f'{central_prefix}[{i}]/td[7]').text.strip()
             results["central"].append({
                 "team": team,
                 "G": game,
@@ -42,17 +42,17 @@ def extract_standings(driver, url_c, url_p):
     
     # --- Pacific League ---
     driver.get(url_p)
-    pacific_prefix = '//*[@id="stdivmaintbl"]/table/tbody/tr/td/div[1]/table/tbody/tr[2]/td[2]'
+    pacific_prefix = '//*[@id="stdivmaintbl"]/table/tbody/tr/td/div[1]/table/tbody/tr'
     pacific_rows = driver.find_elements(By.XPATH, pacific_prefix + '/table/tbody/tr/td')
     for i in range(1, len(pacific_rows)+1):
         try:
-            team = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[2]').text.strip()
-            game = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[3]').text.strip()
-            win = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[4]').text.strip()
-            lose = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[5]').text.strip()
-            tie = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[6]').text.strip()
-            pct = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[7]').text.strip()
-            gb = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[8]').text.strip()
+            team = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[1]table/tbody/tr/td[2]').text.strip()
+            game = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[2]').text.strip()
+            win = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[3]').text.strip()
+            lose = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[4]').text.strip()
+            tie = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[5]').text.strip()
+            pct = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[6]').text.strip()
+            gb = driver.find_element(By.XPATH, f'{pacific_prefix}[{i}]/td[7]').text.strip()
             results["pacific"].append({
                 "team": team,
                 "G": game,
