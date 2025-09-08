ac0d9a5d714ad21af88e9cad8d13e5fae724bb59
Ohmin Kwon <71929898+dhalsdl12@users.noreply.github.com>
2025-09-08 16:49:21 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+NPB_LOGOS = {
+  "central": {
+    "DeNA": "https://npb.jp/bis/images/pet2025_db_1.gif",
+    "Yakult": "https://npb.jp/bis/images/pet2025_s_1.gif",
+    "Chunichi": "https://npb.jp/bis/images/pet2025_d_1.gif",
+    "Yomiuri": "https://npb.jp/bis/images/pet2025_g_1.gif",
+    "Hanshin": "https://npb.jp/bis/images/pet2025_t_1.gif",
+    "Hiroshima": "https://npb.jp/bis/images/pet2025_c_1.gif"
+  },
+  "pacific": {
+    "Seibu": "https://npb.jp/bis/images/pet2025_l_1.gif",
+    "Lotte": "https://npb.jp/bis/images/pet2025_m_1.gif",
+    "ORIX": "https://npb.jp/bis/images/pet2025_b_1.gif",
+    "Nippon-Ham": "https://npb.jp/bis/images/pet2025_f_1.gif",
+    "SoftBank": "https://npb.jp/bis/images/pet2025_h_1.gif",
+    "Rakuten": "https://npb.jp/bis/images/pet2025_e_1.gif"
+  }
+}
+LEAGUE_MAP = {
+    "Central League": "central",
+    "Pacific League": "pacific",
+    "Interleague": "interleague",
+    "C.L. Climax Series First Stage": "climax_central_first",
+    "P.L. Climax Series First Stage": "climax_pacific_first",
+    "C.L. Climax Series Final Stage": "climax_central_final",
+    "P.L. Climax Series Final Stage": "climax_pacific_final",
+    "Nippon Series": "nippon_series"
+}
+LEAGUE_TITLES = {
+    "central": "⚾ 센트럴리그",
+    "pacific": "⚾ 퍼시픽리그",
+    "interleague": "⚾ 인터리그",
+    "climax_central_first": "⚾ C.L. 클라이맥스 시리즈 1차",
+    "climax_pacific_first": "⚾ P.L. 클라이맥스 시리즈 1차",
+    "climax_central_final": "⚾ C.L. 클라이맥스 시리즈 파이널",
+    "climax_pacific_final": "⚾ P.L. 클라이맥스 시리즈 파이널",
+    "nippon_series": "⚾ 일본 시리즈",
+}
