ad338e4338e1522030dd30bf040dfe9eed326eb3
Ohmin Kwon <71929898+dhalsdl12@users.noreply.github.com>
2025-09-08 16:56:22 +0900

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ NPB_LOGOS = {
     "Rakuten": "https://npb.jp/bis/images/pet2025_e_1.gif"
   }
 }
+
 LEAGUE_MAP = {
     "Central League": "central",
     "Pacific League": "pacific",
@@ -26,13 +27,14 @@ LEAGUE_MAP = {
     "P.L. Climax Series Final Stage": "climax_pacific_final",
     "Nippon Series": "nippon_series"
 }
+
 LEAGUE_TITLES = {
-    "central": "⚾ 센트럴리그",
-    "pacific": "⚾ 퍼시픽리그",
-    "interleague": "⚾ 인터리그",
-    "climax_central_first": "⚾ C.L. 클라이맥스 시리즈 1차",
-    "climax_pacific_first": "⚾ P.L. 클라이맥스 시리즈 1차",
-    "climax_central_final": "⚾ C.L. 클라이맥스 시리즈 파이널",
-    "climax_pacific_final": "⚾ P.L. 클라이맥스 시리즈 파이널",
-    "nippon_series": "⚾ 일본 시리즈",
+    "central": "센트럴리그",
+    "pacific": "퍼시픽리그",
+    "interleague": "인터리그",
+    "climax_central_first": "C.L. 클라이맥스 시리즈 1차",
+    "climax_pacific_first": "P.L. 클라이맥스 시리즈 1차",
+    "climax_central_final": "C.L. 클라이맥스 시리즈 파이널",
+    "climax_pacific_final": "P.L. 클라이맥스 시리즈 파이널",
+    "nippon_series": "일본 시리즈",
 }
