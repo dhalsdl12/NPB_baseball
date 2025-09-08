3488609c03b28db68e387d8de260f86e43f57640
Ohmin Kwon <71929898+dhalsdl12@users.noreply.github.com>
2025-09-08 17:16:34 +0900

    Update npb_scores.py

--- npb_scores.py
+++ npb_scores.py
@@ -146,7 +146,7 @@ def generate_md_table(scores):
     for league, games in scores.items():
         title = LEAGUE_TITLES.get(league, league)
 
-        md_contents += f"### ⚾ {title}\n\n"
+        md_contents += f"#### ⚾ {title}\n\n"
 
         if len(games) == 0:
             md_contents += "경기 없음!\n\n"
