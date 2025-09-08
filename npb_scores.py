bcac35a32d81688b6b762d130b3b3997a08957a3
Ohmin Kwon <71929898+dhalsdl12@users.noreply.github.com>
2025-09-08 17:34:14 +0900

    Update npb_scores.py

--- npb_scores.py
+++ npb_scores.py
@@ -25,18 +25,7 @@ def parse_games(driver, rows, base_prefix):
     return games
     
 def extract_scores(driver):
-    central_games = []
-    pacific_games = []
-    results = {
-        "central": [],
-        "pacific": [],
-        "interleague": [],
-        "climax_central_first": [],
-        "climax_pacific_first": [],
-        "climax_central_final": [],
-        "climax_pacific_final": [],
-        "nippon_series": []
-    }
+    results = {key: [] for key in LEAGUE_TITLES.keys()}
     
     # --- 리그 개수 확인 ---
     leagues = driver.find_elements(By.XPATH, '//*[@id="gmdivlist"]/div/table/tbody/tr[1]/td')
@@ -65,87 +54,14 @@ def extract_scores(driver):
 
     return results
     
-    # central = driver.find_elements(By.XPATH, '//*[@id="gmdivlist"]/div/table/tbody/tr[2]/td[1]/div/table/tbody/tr')
-    # pacific = driver.find_elements(By.XPATH, '//*[@id="gmdivlist"]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr')
-    
-    # for i in range(1, len(central) + 1):
-    #     base_xpath = f'//*[@id="gmdivlist"]/div/table/tbody/tr[2]/td[1]/div/table/tbody/tr[{i}]'
-    #     try:
-    #         c_home = driver.find_element(By.XPATH, base_xpath + '/td[2]').text
-    #         c_home_score = driver.find_element(By.XPATH, base_xpath + '/td[3]').text
-    #         c_away_score = driver.find_element(By.XPATH, base_xpath + '/td[5]').text
-    #         c_away = driver.find_element(By.XPATH, base_xpath + '/td[6]').text
-    #         c_score = c_home_score + ' - ' + c_away_score
-            
-    #         central_games.append({
-    #             "home": c_home, 
-    #             "score": c_score, 
-    #             "away": c_away})
-    #     except Exception as e:
-    #         print(f"센트럴 데이터 없음 (row {i}): {e}")
-            
-    # for i in range(1, len(pacific) + 1):
-    #     base_xpath = f'//*[@id="gmdivlist"]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[{i}]'
-    #     try:
-    #         p_home = driver.find_element(By.XPATH, base_xpath + '/td[2]').text
-    #         p_home_score = driver.find_element(By.XPATH, base_xpath + '/td[3]').text
-    #         p_away_score = driver.find_element(By.XPATH, base_xpath + '/td[5]').text
-    #         p_away = driver.find_element(By.XPATH, base_xpath + '/td[6]').text
-    #         p_score = p_home_score + ' - ' + p_away_score
-            
-    #         pacific_games.append({
-    #             "home": p_home, 
-    #             "score": p_score, 
-    #             "away": p_away})
-    #     except Exception as e:
-    #         print(f"퍼시픽 데이터 없음 (row {i}): {e}")
-
-    # return {"central": central_games, "pacific": pacific_games}
-
-
 def generate_md_table(scores):
     """
     여러 경기 리스트를 받아서 HTML 테이블 포함 Markdown 생성
     """
     md_contents = "## 오늘의 경기 결과\n\n\n"
-    
-    # # --- 센트럴리그 ---
-    # md_contents += "## ⚾ 센트럴리그\n\n"
-    # md_contents += "<table>\n"
-    # md_contents += "  <tr>\n    <th></th>\n    <th colspan='3'>센트럴리그</th>\n    <th></th>\n  </tr>\n"
-    # if len(scores['central']) == 0:
-    #     md_contents += "  <tr>\n    <td></td>\n"
-    #     md_contents += "    <td colspan='3'>경기 없음!</td>\n"
-    #     md_contents += "    <td></td>\n  </tr>\n"
-    # else:
-    #     for game in scores['central']:
-    #         home_logo = logos["central"].get(game['home'], "")
-    #         away_logo = logos["central"].get(game['away'], "")
-    #         md_contents += f"  <tr>\n    <td><img src='{home_logo}' width='30'></td>\n"
-    #         md_contents += f"    <td>{game['home']}</td><td>{game['score']}</td><td>{game['away']}</td>\n"
-    #         md_contents += f"    <td><img src='{away_logo}' width='30'></td>\n  </tr>\n"
-    # md_contents += "</table>\n\n"
-
-    # # --- 퍼시픽리그 ---
-    # md_contents += "## ⚾ 퍼시픽리그\n\n"
-    # md_contents += "<table>\n"
-    # md_contents += "  <tr>\n    <th></th>\n    <th colspan='3'>퍼시픽리그</th>\n    <th></th>\n  </tr>\n"
-    # if len(scores['pacific']) == 0:
-    #     md_contents += "  <tr>\n    <td></td>\n"
-    #     md_contents += "    <td colspan='3'>경기 없음!</td>\n"
-    #     md_contents += "    <td></td>\n  </tr>\n"
-    # else:
-    #     for game in scores['pacific']:
-    #         home_logo = logos["pacific"].get(game['home'], "")
-    #         away_logo = logos["pacific"].get(game['away'], "")
-    #         md_contents += f"  <tr>\n    <td><img src='{home_logo}' width='30'></td>\n"
-    #         md_contents += f"    <td>{game['home']}</td><td>{game['score']}</td><td>{game['away']}</td>\n"
-    #         md_contents += f"    <td><img src='{away_logo}' width='30'></td>\n  </tr>\n"
-    # md_contents += "</table>\n\n"
 
     for league, games in scores.items():
         title = LEAGUE_TITLES.get(league, league)
-
         md_contents += f"#### ⚾ {title}\n\n"
 
         if len(games) == 0:
